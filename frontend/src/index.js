0ab9cb7b28a2733d8999243b0dd0caa3969cdac8
prem-y <raopremwaghmare@gmail.com>
2024-03-22 11:38:07 +0530

    Google Auth setup complete

---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <GoogleOAuthProvider clientId="775372399424-vujb76qg26ttmh6i0u4l00fhdojen2e6.apps.googleusercontent.com">
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </GoogleOAuthProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
